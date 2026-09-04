ca0411d00f3f4129e43bb8ba4e3dd0b4a1271fbd
Tan Wei Hong <weihong7811@gmail.com>
2026-09-04 10:54:45 +0800

    second post

--- hello.py
+++ hello.py
@@ -2,3 +2,22 @@ print("================================")
 print("Welcome here")
 print("My first post!")
 print("================================")
+
+username = "cool_creator"
+bio = "Fun Blogger"
+followers = 100
+
+print("Username:", username)
+print("Bio:", bio)
+print("Followers", followers)
+
+followers = 100
+
+followers += 50
+print("Day 1:", followers)
+
+followers += 20
+print("Day 2:", followers)
+
+followers -= 10
+print("Day 3:", followers)
